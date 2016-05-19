742a5b7fc6754d94e21ba1510d5129054f509e72
unknown <dihwang@D-SHC-15008029.corp.ebay.com>
2016-05-19 11:17:44 +0800

    add comparation

---
+++ script/native_pixelated.js
@@ -0,0 +1,12 @@
+
+window.onload = function(){
+	
+	
+	
+
+}
+/**
+ * readme:
+	because bug: https://jira.stubcorp.com/browse/EVENTS-1461		
+	Map lines are pixelated or hard to see in IE and Firefox
+ */
